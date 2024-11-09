bfe1245b298e8dc414bcf7ddf164a8821a03ab9d
Lord-Nickelbottoms <nizaam.h@icloud.com>
2024-11-09 12:45:38 +0200

    added imports to required libraries

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+// used to read JSON data sent from client applications
+const bodyParser = require('body-parser');
+
+// to connect to PostgreSQL
+const pg = require('pg');
