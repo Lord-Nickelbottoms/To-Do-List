221c04de20615781589289c41fadb13eec881a95
Lord-Nickelbottoms <nizaam.h@icloud.com>
2024-11-11 09:59:48 +0200

    added function show the add new task window while hiding various elements

--- scripts/add-new-task-window-script.js
+++ scripts/add-new-task-window-script.js
@@ -1,3 +1,26 @@
 window.addEventListener('DOMContentLoaded', () => {
     console.log('Add new task window script loaded');
 })
+
+const addTaskWindow = document.getElementById("addTaskWindow")
+const taskListWindows = document.getElementById("taskList")
+
+document.getElementById("newTask").addEventListener("click", () => {
+    if (addTaskWindow.style.display === "none") {
+        addTaskWindow.style.display = "block";
+        taskListWindows.style.display = "none";
+    } else {
+        addTaskWindow.style.display = "none";
+        taskListWindows.style.display = "block";
+    }
+})
+
+document.getElementById("closeButton").addEventListener("click", () => {
+    if (addTaskWindow.style.display === "block") {
+        addTaskWindow.style.display = "none";
+        taskListWindows.style.display = "block";
+    } else {
+        addTaskWindow.style.display = "block";
+        taskListWindows.style.display = "none";
+    }
+})
