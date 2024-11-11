800d572b5c6c15a48992a822aac8ce6722fb39fd
Lord-Nickelbottoms <nizaam.h@icloud.com>
2024-11-11 11:46:46 +0200

    added function to add a new task item to the active tasks container.
    added checks tp ensure a title is entered and response to it being empty

---
+++ scripts/task-list-script.js
@@ -0,0 +1,71 @@
+window.addEventListener('DOMContentLoaded', () => {
+    console.log("task-list-script loaded");
+    
+    if (localStorage.length !== 0) {
+        for (let i = 0; i < localStorage.length; i++) {
+            addTask(localStorage.key(i), localStorage.getItem(localStorage.key(i)));
+        }
+    } else {
+        console.log("localStorage empty")
+    }
+})
+
+const activeListContainer = document.getElementById('activeTaskList')
+
+let taskTitle = document.getElementById("title")
+let taskDescription = document.getElementById("description")
+
+document.getElementById('createButton').addEventListener('click', () => {
+    if (taskTitle.innerText !== "") {
+        addTask(taskTitle.value.toString(), taskDescription.value.toString())
+        localStorage.setItem(taskTitle.value.toString(), taskDescription.value.toString())
+    } else {
+        alert("Title cannot be empty.\nAdd a title to continue.")
+    }
+})
+
+// add UI elements for task item
+function addTask(title, description) {
+    const activeTask = document.createElement('div')
+    activeTask.classList.add('task')
+    activeTask.classList.add('active-task')
+    activeListContainer.appendChild(activeTask)
+
+    
+    const completeButton = document.createElement('button')
+    completeButton.classList.add('complete-button')
+
+    const completeIcon = document.createElement('i')
+    completeIcon.classList.add('fa-solid')
+    completeIcon.classList.add('fa-check')
+
+    completeButton.appendChild(completeIcon)
+    activeTask.appendChild(completeButton)
+    
+
+    const taskInformationContainer = document.createElement('div')
+    taskInformationContainer.classList.add('task-information')
+
+    const taskTitle = document.createElement('p')
+    taskTitle.classList.add('task-title')
+    taskTitle.innerText = title
+
+    const taskDescription = document.createElement('p')
+    taskDescription.classList.add('task-description')
+    taskDescription.innerText = description
+
+    taskInformationContainer.appendChild(taskTitle)
+    taskInformationContainer.appendChild(taskDescription)
+    activeTask.appendChild(taskInformationContainer)
+    
+    
+    const deleteButton = document.createElement('button')
+    deleteButton.classList.add('delete-button')
+    
+    const deleteIcon = document.createElement('i')
+    deleteIcon.classList.add('fa-solid')
+    deleteIcon.classList.add('fa-trash-can')
+    
+    deleteButton.appendChild(deleteIcon)
+    activeTask.appendChild(deleteButton)
+}
