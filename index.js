c9527716312285eeff9741a709001ddf47998b32
Lord-Nickelbottoms <nizaam.h@icloud.com>
2024-11-13 22:54:50 +0200

    added CRUD endpoints

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId} = require('mongodb');
 
 // used to read JSON data sent from client applications
 const bodyParser = require('body-parser');
+
+// to bypass cross-origin request errors
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -20,11 +23,64 @@ let toDoCollection;
 const databaseConnection = async  () => {
     try {
         await client.connect()
-        const database = client.db('myDatabase');
+        const database = client.db('ToDoDatabase');
         toDoCollection = database.collection('tasks');
-        console.log("Connected to MongoDB and 'tasks' collection is ready")
+        console.log(`Connected to MongoDB and ${toDoCollection} collection is ready`)
     } catch (error) {
         console.error(`Failed to connect to MongoDB: ${error}`);
     }
 }
 
+// GET request
+app.get('/api/tasks', async (request, response) => {
+    try {
+        const taskList = await toDoCollection.find( {} ).toArray()
+
+        for (let i = 0; i < toDoCollection.length; i++) {
+            console.log('collections: ' + toDoCollection[i])
+            response.end('collections: ' + toDoCollection[i])
+        }
+        
+        response.json(taskList)
+    } catch (e) {
+        response.status(500).json({ error: `An error has occurred: ${e}`})
+    }
+})
+
+// POST request to add a new user
+app.post('/api/new-task', async (request, response) => {
+    const { title, description } = request.body;
+    if (!title) {
+        return response.status(400).json({ error: 'A task title is required' });
+    }
+
+    const newTask = {
+        title,
+        description,
+        status: 'active',
+    };
+
+    try {
+        const result = await toDoCollection.insertOne(newTask);
+        response.status(201).json(result.ops[0]);
+    } catch (err) {
+        response.status(500).json({ error: err.message });
+    }
+});
+
+// DELETE request
+app.delete('/api/task/:id', async (request, response) => {
+    const { id } = request.params;
+    try {
+        const result = await toDoCollection.deleteOne({ _id: new ObjectId(id) });
+        response.json(result);
+    } catch (err) {
+        response.status(500).json({ error: err.message });
+    }
+})
+
+// Start the server and initialize the database
+app.listen(port, async () => {
+    await databaseConnection();
+    console.log(`Server running on http://localhost:${port}`);
+});
